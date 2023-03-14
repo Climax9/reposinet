5207ad3c1cb8c775ff62d5f60e1f6fac01f3d85a
Climax9 <simon325@terpmail.umd.edu>
2023-03-14 19:54:18 -0400

    created changes to packages and modules
    i simon miller created a package with subpackages and an example script

---
+++ presentation.py
@@ -0,0 +1,7 @@
+from information.people.students import student_information
+import people 
+
+if __name__ == "__main__":
+    print("Hi I am currently in the presentation script")
+    student_information("Tim", age=30)
+    people.student_information("Joe", 25)
